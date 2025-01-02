0b94cff428caf2165b66bf5f7276a58c74bf0ff7
Jannat Supti <jannatsupti61@gmail.com>
2025-01-02 13:50:41 +0600

    Auth-Service:Add DTO and Entities

---
+++ auth-service/src/main/java/tech/talenthium/authservice/dto/event/UserCreatedEvent.java
@@ -0,0 +1,19 @@
+package tech.talenthium.authservice.dto.event;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import tech.talenthium.authservice.entity.Role;
+
+import java.time.Instant;
+@Getter
+@AllArgsConstructor
+@Builder
+public class UserCreatedEvent {
+    Long userId;
+    String email;
+    String username;
+    String name;
+    Role role;
+    Instant createdAt;
+}
